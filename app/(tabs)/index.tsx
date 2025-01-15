ab9ca6e207c55f9182fd8318ea9f4decdc638de6
ambga112 <ambga112@student.liu.se>
2025-01-15 17:24:13 +0100

    final

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -31,7 +31,7 @@ export default function HomeScreen() {
               web: 'F12'
             })}
           </ThemedText>{' '}
-          to open developer tools.
+          to open developer tool.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
